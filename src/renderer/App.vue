2e6553099f8ae310f210e21d9ad7ff6ca2b2b376
expecter <1005426244@qq.com>
2019-04-19 09:13:25 +0800

    111

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -7,6 +7,16 @@
         </ul>
       </el-aside>
       <el-main>
+        <el-dialog
+          title="提示"
+          :visible.sync="dialogVisible"
+          width="30%">
+          <span>这是一段信息</span>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+          </span>
+        </el-dialog>
         <router-view></router-view>
       </el-main>
     </el-container>
@@ -26,6 +36,7 @@
     },
     data () {
       return {
+        dialogVisible: true,
         links: [
           {
             text: '日历',
