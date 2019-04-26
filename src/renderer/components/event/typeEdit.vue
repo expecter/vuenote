cad0cbccca25321fd77b034119c2a95ccd2222b1
expecter <1005426244@qq.com>
2019-04-26 18:54:49 +0800

    111

--- src/renderer/components/event/typeEdit.vue
+++ src/renderer/components/event/typeEdit.vue
@@ -4,28 +4,35 @@
   :visible.sync="mgshowDialog"
   width="60%">
   <div>
-    <el-form ref="form" :model="form" label-width="80px">    
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="活动类型">
+        <el-select v-model = 'locale' placeholder="请选择活动区域" style="width: 100%;">
+          <el-option  v-for="item in localesList" :label="item.name" :value="item.type" :key="item.type"></el-option>
+        </el-select>
+      </el-form-item>
     </el-form>
   </div>
 </el-dialog>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        mgshowDialog: false,
-        form: {}
-      }
-    },
-    props: ['showTypeDialog'],
-    watch: {
-      showTypeDialog () {
-        this.mgshowDialog = true
-      }
-    },
-    methods: {
-      onSubmit () {
-      }
+import config from '@/components/config/config'
+export default {
+  data () {
+    return {
+      localesList: config.workData(),
+      mgshowDialog: false,
+      form: {}
+    }
+  },
+  props: ['showTypeDialog'],
+  watch: {
+    showTypeDialog () {
+      this.mgshowDialog = true
+    }
+  },
+  methods: {
+    onSubmit () {
     }
   }
+}
 </script>
