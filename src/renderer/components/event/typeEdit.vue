aec991f694ac06286b176ea900ad0420f8bc522b
expecter <1005426244@qq.com>
2019-04-27 14:26:58 +0800

    111

--- src/renderer/components/event/typeEdit.vue
+++ src/renderer/components/event/typeEdit.vue
@@ -4,13 +4,25 @@
   :visible.sync="mgshowDialog"
   width="60%">
   <div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动类型">
-        <el-select v-model = 'locale' placeholder="请选择活动区域" style="width: 100%;">
-          <el-option  v-for="item in localesList" :label="item.name" :value="item.type" :key="item.type"></el-option>
-        </el-select>
-      </el-form-item>
-    </el-form>
+    <el-tag
+    :key="tag"
+    v-for="tag in dynamicTags"
+    closable
+    :disable-transitions="false"
+    @close="handleClose(tag)">
+    {{tag}}
+  </el-tag>
+  <el-input
+    class="input-new-tag"
+    v-if="inputVisible"
+    v-model="inputValue"
+    ref="saveTagInput"
+    size="small"
+    @keyup.enter.native="handleInputConfirm"
+    @blur="handleInputConfirm"
+  >
+  </el-input>
+  <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
   </div>
 </el-dialog>
 </template>
@@ -22,7 +34,11 @@ export default {
       localesList: config.workData(),
       mgshowDialog: false,
       locale: '',
-      form: {}
+      msg: '',
+      form: {},
+      dynamicTags: ['标签一', '标签二', '标签三'],
+      inputVisible: false,
+      inputValue: ''
     }
   },
   props: ['showTypeDialog'],
@@ -32,8 +48,42 @@ export default {
     }
   },
   methods: {
-    onSubmit () {
+    handleClose (tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+    },
+
+    showInput () {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+
+    handleInputConfirm () {
+      let inputValue = this.inputValue
+      if (inputValue) {
+        this.dynamicTags.push(inputValue)
+      }
+      this.inputVisible = false
+      this.inputValue = ''
     }
   }
 }
 </script>
+<style>
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
+</style>
